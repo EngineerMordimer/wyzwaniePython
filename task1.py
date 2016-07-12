501b686814dd6eae11ce73f49c8928d12c615f80
EngineerMordimer <s12103@pjwstk.edu.pl>
2016-07-12 15:09:19 +0200

    Small troubleshooting

--- task1.py
+++ task1.py
@@ -1,20 +1,19 @@
+import sys
 import os
 import re
 import time
 import shutil
 
-print('START')
-localPath = os.getcwd()
+try:
+    localPath = sys.argv[1]
+except IndexError:
+    localPath = os.getcwd()
 Files = [[ele, time.gmtime(os.path.getmtime(ele))] for ele in os.listdir(localPath) if (not (re.match('(.*).py', ele) or re.match('\.(.*)', ele)) and os.path.isfile(localPath + '/' + ele))]
 i = 0
 for file in Files:
     fileName = file[0]
     newFilePath = localPath + '/' + time.strftime('%Y', file[1]) + '/' + time.strftime('%m', file[1]) + '/'
-    i += 1
-    try:
-        original_umask = os.umask(0)
-        os.makedirs(newFilePath, mode=0o777, exist_ok=True)
-    finally:
-        os.umask(original_umask)
+    os.makedirs(newFilePath, mode=0o755, exist_ok=True)
     shutil.copy2(localPath + '/' + fileName, newFilePath + fileName)
+    i += 1
 print(str(i) + ' files copied')
