21bedfda5fe5f84e4e4dc0cf7571ba3644676c47
EngineerMordimer <s12103@pjwstk.edu.pl>
2016-07-20 13:47:06 +0200

    correction after comment

--- task1.py
+++ task1.py
@@ -4,16 +4,30 @@ import re
 import time
 import shutil
 
+
+def is_proper_file(ele, path ):
+    """This method test element if
+    1. it is NOT a element with '.py' at end of name
+    2. it is NOT a element with '.' at start of name
+    3. ele is a file in path
+    """
+    if not (re.match('(.*).py', ele) or re.match('\.(.*)', ele)) and os.path.isfile(path + '/' + ele):
+        return 1
+    else:
+        return None
 try:
     localPath = sys.argv[1]
 except IndexError:
     localPath = os.getcwd()
-Files = [[ele, time.gmtime(os.path.getmtime(ele))] for ele in os.listdir(localPath) if (not (re.match('(.*).py', ele) or re.match('\.(.*)', ele)) and os.path.isfile(localPath + '/' + ele))]
-i = 0
-for file in Files:
+files = [[ele, time.gmtime(os.path.getmtime(ele))] for ele in os.listdir(localPath) if is_proper_file(ele, localPath)]
+for file in files:
     fileName = file[0]
-    newFilePath = localPath + '/' + time.strftime('%Y', file[1]) + '/' + time.strftime('%m', file[1]) + '/'
-    os.makedirs(newFilePath, mode=0o755, exist_ok=True)
-    shutil.copy2(localPath + '/' + fileName, newFilePath + fileName)
-    i += 1
-print(str(i) + ' files copied')
+    yearMod = time.strftime('%Y', file[1])
+    monthMod = time.strftime('%m', file[1])
+    newFileDirectory = '{localPath}/{year}/{month}/'.format(localPath=localPath, year=yearMod, month=monthMod)
+    newFilePath = '{newFileDirectory}/{fileName}'.format(newFileDirectory=newFileDirectory, fileName=fileName)
+    oldFilePath = '{localPath}/{fileName}'.format(localPath=localPath, fileName=fileName)
+    os.makedirs(newFileDirectory, mode=0o755, exist_ok=True)
+    shutil.copy2(oldFilePath, newFilePath)
+
+print(str(len(files)) + ' files copied')
