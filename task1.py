245a1bb5c188c63732a18d0783d74e7bca6c931a
EngineerMordimer <s12103@pjwstk.edu.pl>
2016-07-20 14:31:25 +0200

    correction after comment on github

--- task1.py
+++ task1.py
@@ -3,6 +3,7 @@ import os
 import re
 import time
 import shutil
+import collections
 
 
 def is_proper_file(ele, path ):
@@ -11,23 +12,25 @@ def is_proper_file(ele, path ):
     2. it is NOT a element with '.' at start of name
     3. ele is a file in path
     """
-    if not (re.match('(.*).py', ele) or re.match('\.(.*)', ele)) and os.path.isfile(path + '/' + ele):
-        return 1
-    else:
-        return None
-try:
-    localPath = sys.argv[1]
-except IndexError:
-    localPath = os.getcwd()
-files = [[ele, time.gmtime(os.path.getmtime(ele))] for ele in os.listdir(localPath) if is_proper_file(ele, localPath)]
+    return not (re.match('(.*).py', ele) or re.match('\.(.*)', ele)) and os.path.isfile(path + '/' + ele)
+
+
+def get_modification_time(ele):
+    """Return modification time of file in
+        (tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst)"""
+    return time.gmtime(os.path.getmtime(ele))
+
+
+localPath = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
+FileElements = collections.namedtuple('FileElements', 'fileName modDate')
+files = [FileElements(ele, get_modification_time(ele)) for ele in os.listdir(localPath) if is_proper_file(ele, localPath)]
 for file in files:
-    fileName = file[0]
-    yearMod = time.strftime('%Y', file[1])
-    monthMod = time.strftime('%m', file[1])
-    newFileDirectory = '{localPath}/{year}/{month}/'.format(localPath=localPath, year=yearMod, month=monthMod)
-    newFilePath = '{newFileDirectory}/{fileName}'.format(newFileDirectory=newFileDirectory, fileName=fileName)
-    oldFilePath = '{localPath}/{fileName}'.format(localPath=localPath, fileName=fileName)
+    fileName = file.fileName
+    yearMod, monthMod = time.strftime('%Y', file.modDate), time.strftime('%m', file.modDate)
+    newFileDirectory = '{localPath}/{yearMod}/{monthMod}/'.format(**vars())
+    newFilePath = '{newFileDirectory}/{fileName}'.format(**vars())
+    oldFilePath = '{localPath}/{fileName}'.format(**vars())
     os.makedirs(newFileDirectory, mode=0o755, exist_ok=True)
     shutil.copy2(oldFilePath, newFilePath)
 
-print(str(len(files)) + ' files copied')
+print(len(files), ' files copied')
